babecdbd015c00cba4ed823725ab4f84bc66a71f
livia <livia.soffi@cern.ch>
2017-12-04 19:19:32 +0100

    Integration of Fall17 VID - Cutbased

--- RecoEgamma/PhotonIdentification/plugins/PhotonIDValueMapProducer.cc
+++ RecoEgamma/PhotonIdentification/plugins/PhotonIDValueMapProducer.cc
@@ -67,7 +67,7 @@ class PhotonIDValueMapProducer : public edm::stream::EDProducer<> {
   float computeWorstPFChargedIsolation(const T& photon,
 				       const U& pfCandidates,
 				       const edm::Handle<reco::VertexCollection> vertices,
-				       bool isAOD,
+				       bool isAOD, const reco::Vertex pv,
 				       float dRmax, float dxyMax, float dzMax,
 				       float dRvetoBarrel, float dRvetoEndcap, float ptMin);
 
@@ -77,6 +77,9 @@ class PhotonIDValueMapProducer : public edm::stream::EDProducer<> {
   candidatePdgId(const edm::Ptr<reco::Candidate> candidate, bool isAOD);
 
   const reco::Track* getTrackPointer(const edm::Ptr<reco::Candidate> candidate, bool isAOD);
+  void getImpactParameters(const edm::Ptr<reco::Candidate> candidate,
+                           bool isAOD, const reco::Vertex pv, float &dxy, float &dz);
+
 
 
   // The object that will compute 5x5 quantities  
@@ -118,6 +121,11 @@ class PhotonIDValueMapProducer : public edm::stream::EDProducer<> {
   constexpr static char phoWorstChargedIsolation_[] = "phoWorstChargedIsolation";
   constexpr static char phoWorstChargedIsolationWithConeVeto_[] = "phoWorstChargedIsolationWithConeVeto";
 
+  //PFCluster Isolation
+  constexpr static char phoTrkIsolation_[] = "phoTrkIsolation";
+  constexpr static char phoHcalPFClIsolation_[] = "phoHcalPFClIsolation";
+  constexpr static char phoEcalPFClIsolation_[] = "phoEcalPFClIsolation";
+
 };
 
 // Cluster shapes
@@ -134,6 +142,12 @@ constexpr char PhotonIDValueMapProducer::phoNeutralHadronIsolation_[];
 constexpr char PhotonIDValueMapProducer::phoPhotonIsolation_[];
 constexpr char PhotonIDValueMapProducer::phoWorstChargedIsolation_[];
 constexpr char PhotonIDValueMapProducer::phoWorstChargedIsolationWithConeVeto_[];
+
+//PFCluster Isolation
+constexpr char PhotonIDValueMapProducer::phoTrkIsolation_[];
+constexpr char PhotonIDValueMapProducer::phoHcalPFClIsolation_[];
+constexpr char PhotonIDValueMapProducer::phoEcalPFClIsolation_[];
+
 
 PhotonIDValueMapProducer::PhotonIDValueMapProducer(const edm::ParameterSet& iConfig) {
 
@@ -197,6 +211,12 @@ PhotonIDValueMapProducer::PhotonIDValueMapProducer(const edm::ParameterSet& iCon
   produces<edm::ValueMap<float> >(phoPhotonIsolation_);  
   produces<edm::ValueMap<float> >(phoWorstChargedIsolation_);  
   produces<edm::ValueMap<float> >(phoWorstChargedIsolationWithConeVeto_);  
+
+  //PFCluster  Isolations
+  produces<edm::ValueMap<float> >(phoTrkIsolation_);  
+  produces<edm::ValueMap<float> >(phoHcalPFClIsolation_);  
+  produces<edm::ValueMap<float> >(phoEcalPFClIsolation_);  
+
 
 }
 
@@ -292,6 +312,11 @@ void PhotonIDValueMapProducer::produce(edm::Event& iEvent, const edm::EventSetup
   std::vector<float> phoPhotonIsolation;
   std::vector<float> phoWorstChargedIsolation;
   std::vector<float> phoWorstChargedIsolationWithConeVeto;
+
+  //PFCluster Isolations
+  std::vector<float> phoTrkIsolation;
+  std::vector<float> phoHcalPFClIsolation;
+  std::vector<float> phoEcalPFClIsolation;
   
   // reco::Photon::superCluster() is virtual so we can exploit polymorphism
   for (unsigned idxpho = 0; idxpho < src->size(); ++idxpho) {
@@ -328,6 +353,21 @@ void PhotonIDValueMapProducer::produce(edm::Event& iEvent, const edm::EventSetup
     math::XYZVector photon_directionWrtVtx(iPho->superCluster()->x() - pv.x(),
                                            iPho->superCluster()->y() - pv.y(),
                                            iPho->superCluster()->z() - pv.z());
+
+    //PFCluster Isolations
+    phoTrkIsolation      .push_back( iPho->trkSumPtSolidConeDR04());
+    if (isAOD)                                                                                                                                                                  
+      {                                                                                                                                                                          
+	phoHcalPFClIsolation .push_back(0.f);
+	phoEcalPFClIsolation .push_back(0.f);
+      }
+    else
+      {
+	edm::Ptr<pat::Photon> patPhotonPtr(src->ptrAt(idxpho));
+	phoHcalPFClIsolation .push_back(patPhotonPtr->hcalPFClusterIso());
+	phoEcalPFClIsolation .push_back(patPhotonPtr->ecalPFClusterIso());
+      }
+
 
     // Zero the isolation sums
     float chargedIsoSum = 0;
@@ -378,12 +418,16 @@ void PhotonIDValueMapProducer::produce(edm::Event& iEvent, const edm::EventSetup
       if( thisCandidateType == reco::PFCandidate::h ){
 	// for charged hadrons, additionally check consistency
 	// with the PV
-	const reco::Track *theTrack = getTrackPointer( iCand, isAOD );
+	float dxy = -999, dz=-999;
+        getImpactParameters(iCand, isAOD, pv, dxy, dz);
 
-	float dxy = theTrack->dxy(pv.position());
+	//
+	//const reco::Track *theTrack = getTrackPointer( iCand, isAOD );
+	//
+	//float dxy = theTrack->dxy(pv.position());
 	if(fabs(dxy) > dxyMax) continue;
-
-	float dz  = theTrack->dz(pv.position());
+	//
+	//float dz  = theTrack->dz(pv.position());
 	if (fabs(dz) > dzMax) continue;
 
 	// The candidate is eligible, increment the isolaiton
@@ -408,7 +452,7 @@ void PhotonIDValueMapProducer::produce(edm::Event& iEvent, const edm::EventSetup
     float ptMin = 0.0;
     float worstChargedIso =
       computeWorstPFChargedIsolation(iPho, pfCandidatesHandle, vertices, 
-				     isAOD, coneSizeDR, dxyMax, dzMax,
+				     isAOD, pv,coneSizeDR, dxyMax, dzMax,
 				     dRvetoBarrel, dRvetoEndcap, ptMin);
     phoWorstChargedIsolation .push_back( worstChargedIso );
 
@@ -419,7 +463,7 @@ void PhotonIDValueMapProducer::produce(edm::Event& iEvent, const edm::EventSetup
     ptMin = 0.1;
     float worstChargedIsoWithConeVeto =
       computeWorstPFChargedIsolation(iPho, pfCandidatesHandle, vertices, 
-				     isAOD, coneSizeDR, dxyMax, dzMax,
+				     isAOD, pv, coneSizeDR, dxyMax, dzMax,
 				     dRvetoBarrel, dRvetoEndcap, ptMin);
     phoWorstChargedIsolationWithConeVeto .push_back( worstChargedIsoWithConeVeto );
 
@@ -435,12 +479,17 @@ void PhotonIDValueMapProducer::produce(edm::Event& iEvent, const edm::EventSetup
   writeValueMap(iEvent, src, phoFull5x5E2x5Max, phoFull5x5E2x5Max_);  
   writeValueMap(iEvent, src, phoFull5x5E5x5   , phoFull5x5E5x5_);  
   writeValueMap(iEvent, src, phoESEffSigmaRR  , phoESEffSigmaRR_);  
-  // IsolationsOB
+  // Isolation
   writeValueMap(iEvent, src, phoChargedIsolation, phoChargedIsolation_);  
   writeValueMap(iEvent, src, phoNeutralHadronIsolation, phoNeutralHadronIsolation_);  
   writeValueMap(iEvent, src, phoPhotonIsolation, phoPhotonIsolation_);  
   writeValueMap(iEvent, src, phoWorstChargedIsolation, phoWorstChargedIsolation_);  
   writeValueMap(iEvent, src, phoWorstChargedIsolationWithConeVeto, phoWorstChargedIsolationWithConeVeto_);  
+  //PFCluster  Isolation
+  writeValueMap(iEvent, src, phoTrkIsolation, phoTrkIsolation_);  
+  writeValueMap(iEvent, src, phoHcalPFClIsolation, phoHcalPFClIsolation_);  
+  writeValueMap(iEvent, src, phoEcalPFClIsolation, phoEcalPFClIsolation_);  
+
 }
 
 void PhotonIDValueMapProducer::writeValueMap(edm::Event &iEvent,
@@ -471,7 +520,7 @@ template <class T, class U>
 float PhotonIDValueMapProducer
 ::computeWorstPFChargedIsolation(const T& photon, const U& pfCandidates,
 				 const edm::Handle<reco::VertexCollection> vertices,
-				 bool isAOD,
+				 bool isAOD,const reco::Vertex pv,
 				 float dRmax, float dxyMax, float dzMax,
 				 float dRvetoBarrel, float dRvetoEndcap, float ptMin){
 
@@ -508,11 +557,15 @@ float PhotonIDValueMapProducer
       if (iCand->pt() < ptMin)
 	continue;
       
-      const reco::Track *theTrack = getTrackPointer( iCand, isAOD );
-      float dxy = theTrack->dxy(vtx->position());
+      float dxy=-999, dz=-999;
+      getImpactParameters(iCand, isAOD, pv, dxy, dz);
+
+
+      //      const reco::Track *theTrack = getTrackPointer( iCand, isAOD );
+      //float dxy = theTrack->dxy(vtx->position());
       if( fabs(dxy) > dxyMax) continue;
-      
-      float dz = theTrack->dz(vtx->position());
+      //
+      //float dz = theTrack->dz(vtx->position());
       if ( fabs(dz) > dzMax) continue;
       
       float dR2 = deltaR2(photon_directionWrtVtx.Eta(), photon_directionWrtVtx.Phi(), 
@@ -562,6 +615,25 @@ PhotonIDValueMapProducer::getTrackPointer(const edm::Ptr<reco::Candidate> candid
     theTrack = &( ((const patCandPtr) candidate)->pseudoTrack());
 
   return theTrack;
+}
+
+void PhotonIDValueMapProducer::getImpactParameters(const edm::Ptr<reco::Candidate> candidate,
+                                                   bool isAOD, const reco::Vertex pv,
+                                                   float &dxy, float &dz){
+
+  dxy=-999;
+  dz=-999;
+  if( isAOD ) {
+    const reco::Track *theTrack = &*( ((const recoCandPtr) candidate)->trackRef());
+    dxy = theTrack->dxy(pv.position());
+    dz  = theTrack->dz(pv.position());
+  } else {
+    dxy = ((const patCandPtr) candidate)->dxy(pv.position());
+    dz = ((const patCandPtr) candidate)->dz(pv.position());
+
+  }
+
+  return;
 }
 
 DEFINE_FWK_MODULE(PhotonIDValueMapProducer);
